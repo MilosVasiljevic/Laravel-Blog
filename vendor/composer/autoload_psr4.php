61936f8858b196e92949445ba7b37dd891980c36
MilosVasiljevic <MilosVasiljevicDev@outlook.com>
2025-05-14 15:36:07 +0200

    Changed php version, faker

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -62,7 +62,6 @@ return array(
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'Fruitcake\\Cors\\' => array($vendorDir . '/fruitcake/laravel-cors/src'),
     'Fideloper\\Proxy\\' => array($vendorDir . '/fideloper/proxy/src'),
-    'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
     'Facade\\Ignition\\' => array($vendorDir . '/facade/ignition/src'),
     'Facade\\IgnitionContracts\\' => array($vendorDir . '/facade/ignition-contracts/src'),
     'Facade\\FlareClient\\' => array($vendorDir . '/facade/flare-client-php/src'),
